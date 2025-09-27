4ca374f5184bc7c827d3ec78d0823cc32cbb551b
Karl Seyram <karlseyram2@gmail.com>
2025-09-27 15:44:46 +0000

    Try fixing this error: `Console Error: Error: Supabase URL is not set or

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { config } from 'dotenv';
-
-config();
 
 // For use in server-side logic (e.g., API routes, Server Actions)
 export function createAdminClient() {
