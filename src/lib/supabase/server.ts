4718aa1d0234e7862ed6d884f2892dd16c8ce408
Karl Seyram <karlseyram2@gmail.com>
2025-09-27 16:25:54 +0000

    Try fixing this error: `Console Error: Supabase Service Role Key is not

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,6 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js';
-import 'dotenv/config';
 
 // For use in server-side logic (e.g., API routes, Server Actions)
 export function createAdminClient() {
