8367f529a0243e418e38ffb98eccfc7c4be46b30
Karl Seyram <karlseyram2@gmail.com>
2025-09-30 01:07:59 +0000

    make this the favicon (_for element svg_)

--- src/components/admin/admin-sidebar.tsx
+++ src/components/admin/admin-sidebar.tsx
@@ -8,7 +8,6 @@ import {
   UploadCloud,
   Inbox,
   Home,
-  BookOpenCheck,
 } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import {
@@ -34,7 +33,7 @@ export function AdminSidebar() {
     <>
       <SidebarHeader>
         <div className="flex items-center gap-2">
-          <BookOpenCheck className="h-6 w-6 text-primary" />
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className="h-6 w-6 text-primary"><path d='M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20'/></svg>
           <span className="font-headline text-lg font-bold">CyberShelf</span>
         </div>
       </SidebarHeader>
