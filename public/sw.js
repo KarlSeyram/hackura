ed5811a76afe088cf7989863ccfc5af79a16828b
Karl Seyram <karlseyram2@gmail.com>
2025-10-02 10:48:48 +0000

    commit

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,3 @@
-// Paste the content of the sw.js file provided by Monetag here.
-// It will likely look something like: importScripts("https://some-cdn.com/script.js");
 self.options = {
     "domain": "upskittyan.com",
     "zoneId": 9961416
