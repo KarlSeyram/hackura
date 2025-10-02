0d165616726b5874a2523cc03fb48809f3f42b2c
Karl Seyram <karlseyram2@gmail.com>
2025-10-02 10:27:19 +0000

    added ads

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,8 @@
 // Paste the content of the sw.js file provided by Monetag here.
 // It will likely look something like: importScripts("https://some-cdn.com/script.js");
+self.options = {
+    "domain": "upskittyan.com",
+    "zoneId": 9961416
+}
+self.lary = ""
+importScripts('https://upskittyan.com/act/files/service-worker.min.js?r=sw')
