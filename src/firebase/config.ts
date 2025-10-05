2de547ba2d813fb8e9c37440ccae107157ed0ece
Karl Seyram <karlseyram2@gmail.com>
2025-10-05 00:34:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7786081940-95269",
+  "appId": "1:213371076971:web:46b9e80424911228c30c2e",
+  "apiKey": "AIzaSyB7NdbDgKR2iw-oAlbOuNhm3SPR_bnx0Dc",
+  "authDomain": "studio-7786081940-95269.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "213371076971"
+};
