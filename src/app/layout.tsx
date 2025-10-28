0d48ae1cf337af19a7ebe78d3fde006d155e832c
Karl Seyram <karlseyram2@gmail.com>
2025-10-28 17:38:32 +0000

    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsby

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3524835634132252"
+     crossOrigin="anonymous"></script>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
